3c37c21d415fe0d4a6192bf587645403d65d7c6b
anggaihza <angga.asep98@gmail.com>
2023-05-19 10:43:07 +0700

    final fix

--- pages/search.js
+++ pages/search.js
@@ -6,7 +6,8 @@ import { BsFilter } from "react-icons/bs"
 import SearchFilters from "@/components/SearchFilters";
 import Property from "@/components/Property";
 import noresult from "../assets/images/noresult.svg"
-import { fetchApi, baseUrl } from "@/utils/fetchApi";
+
+import { baseUrl, fetchApi } from "@/utils/fetchApi";
 
 const Search = ({ properties }) => {
     const [searchFilters, setSearchFilters] = useState(false)
