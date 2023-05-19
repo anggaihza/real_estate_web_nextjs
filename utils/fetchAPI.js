57d1634e961dcc25d7841c28013befb192b3ad08
anggaihza <angga.asep98@gmail.com>
2023-05-19 08:07:29 +0700

    final commit three

--- utils/fetchAPI.js
+++ utils/fetchAPI.js
@@ -1,13 +1,14 @@
 import axios from "axios";
 
-export const baseUrl = 'https://bayut.p.rapidapi.com'
+export const baseUrl = 'https://bayut.p.rapidapi.com';
 
 export const fetchApi = async (url) => {
     const { data } = await axios.get((url), {
         headers: {
-            'X-RapidAPI-Key': '32030df0e5msh1b7786155b1484cp16971fjsna7123886bdcb',
-            'X-RapidAPI-Host': 'bayut.p.rapidapi.com'
-        }
+            'x-rapidapi-host': 'bayut.p.rapidapi.com',
+            'x-rapidapi-key': process.env.NEXT_PUBLIC_RAPID_API_KEY,
+        },
     });
-    return data
+
+    return data;
 }
